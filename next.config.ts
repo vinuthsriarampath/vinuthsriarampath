b7615a85c685d425c71c15d7f6e33c39f58863d7
vinuth <vinuthsriarampath@outlook.com>
2025-07-22 08:54:37 +0530

    refactor: update image configuration to use remote patterns for improved flexibility

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: [process.env.NEXT_PUBLIC_SUPABASE_DOMAIN || ''],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_SUPABASE_DOMAIN || '',
+      },
+    ],
   },
 };
 
