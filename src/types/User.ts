58b883c949f3dec3afab49d89f12b0b940da7892
vinuth <vinuthsriarampath@outlook.com>
2025-09-05 09:04:20 +0530

    chore: add User type

---
+++ src/types/User.ts
@@ -0,0 +1,12 @@
+export interface User{
+    id : number
+    first_name : string
+    last_name : string
+    contact : string
+    dob : Date
+    address : string
+    postal_code : number
+    email : string
+    bio? : string 
+    avatar? : string
+}
